39aa52afacb181dea7a0ac622c88b679cccfd7a7
J. Ricardo Pérez Castillo <alu0100832976@ull.edu.es>
2017-04-25 10:36:47 +0100

    Ya muestra algo la disparidad

--- src/disparity_BM.cpp
+++ src/disparity_BM.cpp
@@ -11,28 +11,31 @@
 #include "message_filters/sync_policies/approximate_time.h"
 #include "message_filters/subscriber.h"
 
-typedef message_filters::sync_policies::ApproximateTime<sensor_msgs::Image, sensor_msgs::Image> MySyncPolicy;
 static const std::string OPENCV_WINDOWD = "Disparity";
+
+typedef message_filters::sync_policies::ApproximateTime<
+      sensor_msgs::Image, sensor_msgs::Image
+      > MySyncPolicy;
 
 class DisparityBM
 {
 private:
-   ros::NodeHandle node_obj_;
-   image_transport::SubscriberFilter image_left_subscriber_;
-   image_transport::SubscriberFilter image_right_subscriber_;
-   image_transport::Publisher disparity_bm_publisher_;
+   ros::NodeHandle nh_;
    image_transport::ImageTransport it_;
-   message_filters::Synchronizer< MySyncPolicy > sync_;
+   image_transport::SubscriberFilter image_left_sub_;
+   image_transport::SubscriberFilter image_right_sub_;
+   image_transport::Publisher disparity_bm_pub_;
+   message_filters::Synchronizer<MySyncPolicy> sync_;
 
 public:
    DisparityBM()
-   : it_(node_obj_),
-     image_left_subscriber_(it_, "/stereo/left/image_rect", 1),
-     image_right_subscriber_(it_, "/stereo/right/image_rect", 1),
-     sync_(MySyncPolicy(10), image_left_subscriber_, image_right_subscriber_)
+   : it_(nh_),
+     image_left_sub_(it_, "/stereo/left/image_rect", 1),
+     image_right_sub_(it_, "/stereo/right/image_rect", 1),
+     sync_(MySyncPolicy(100),image_left_sub_, image_right_sub_)
    {
       sync_.registerCallback(boost::bind(&DisparityBM::disparity_callback, this, _1, _2));
-      disparity_bm_publisher_ = it_.advertise("/disparity/bm", 1);    
+      disparity_bm_pub_ = it_.advertise("/disparity/bm", 1);    
       cv::namedWindow(OPENCV_WINDOWD);
    }
 
@@ -49,14 +52,14 @@ public:
       cv_bridge::CvImageConstPtr cv_ptr_right = cv_bridge::toCvShare(image_right_msg, sensor_msgs::image_encodings::TYPE_8UC1);
       const cv::Mat left_image = cv_ptr_left->image;
       const cv::Mat right_image = cv_ptr_right->image;
+      const cv::Mat imgDisparity16S = cv::Mat(left_image.rows, left_image.cols, CV_16S);
+      const cv::Mat imgDisparity8U = cv::Mat(left_image.rows, left_image.cols, CV_8UC1);
 
-      cv::Mat imgDisparity8U = cv::Mat(left_image.rows, left_image.cols, CV_8UC1);
-
-      int ndisparity = 0;
+      int ndisparity = 16;
       int SADWindowSize = 21;
       cv::Ptr<cv::StereoBM> sbm = cv::StereoBM::create(ndisparity, SADWindowSize);
 
-      sbm->compute(left_image, right_image, imgDisparity8U);
+      sbm->compute(left_image, right_image, imgDisparity16S);
 
       double minVal;
       double maxVal;
@@ -65,7 +68,7 @@ public:
 
       ROS_INFO("Min disp: %f Max value: %f", minVal, maxVal);
 
-      //imgDisparity16S.convertTo(imgDisparity8U, CV_8UC1, 255/(maxVal - minVal));
+      imgDisparity16S.convertTo(imgDisparity8U, CV_8UC1, 255/(maxVal - minVal));
 
       cv::imshow(OPENCV_WINDOWD, imgDisparity8U);
       cv::waitKey(3);
@@ -78,7 +81,6 @@ int main(int argc, char **argv)
 {
    ros::init(argc, argv, "DisparityBM");
    DisparityBM dbm;
-   while(ros::ok())
-      ros::spin();
+   ros::spin();
    return 0;
 }
